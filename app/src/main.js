ae8a32297614b2665fdf34f3bc2900846369d3bb
zane <zane@zanedev.com>
2014-06-02 20:41:05 -0700

    simple album artwork loading

--- app/src/main.js
+++ app/src/main.js
@@ -1,23 +1,15 @@
-/*globals define*/
 define(function(require, exports, module) {
-    'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
-    var StateModifier = require('famous/modifiers/StateModifier');
+   
+	var AppView = require('views/AppView');
 
-    // create the main context
-    var mainContext = Engine.createContext();
+	var mainCtx = Engine.createContext();
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png'
-    });
+	var appView = new AppView();
 
-    var logoModifier = new StateModifier({
-        origin: [0.5, 0.5]
-    });
+	mainCtx.add(appView);
 
-    mainContext.add(logoModifier).add(logo);
+	mainCtx.setPerspective(1);
+
 });
